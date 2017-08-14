80a77ae62937782070f1c5841a6d9d51aee569ec
Kamil Sałęga <kamilsalega@gmail.com>
2017-08-14 17:39:05 +0200

    Utworzenie userfrontend

---
+++ userfrontend/src/test/java/pl/salega/userfrontend/UserFrontendApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.salega.userfrontend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserFrontendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
